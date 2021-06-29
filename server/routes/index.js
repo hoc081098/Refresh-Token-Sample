18526df9b39081c4d24e1fcba84ac07442668030
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-06-29 17:17:12 +0700

    init server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
+const jwt = require('jsonwebtoken');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+const secret = 'my-secret';
+const expiresIn = '1m';
+
+const refreshSecret = 'my-refresh-secret';
+const refreshExpiresIn = '1d';
+
+const refreshTokensSet = new Set();
+
+router.post('/login', function (req, res, next) {
+  const { username, password } = req.body;
+  if (username === 'hoc081098' && password === '123456') {
+    const payload = {
+      id: '#1',
+      username,
+    };
+
+    const token = jwt.sign(
+      payload,
+      secret,
+      { expiresIn },
+    );
+
+    const refreshToken = jwt.sign(
+      payload,
+      refreshSecret,
+      { expiresIn: refreshExpiresIn },
+    );
+
+    res.status(200).json({
+      token,
+      refreshToken,
+      ...payload,
+    });
+  } else {
+    res.status(401);
+  }
+});
+
+router.post('/refresh-token', function (req, res, next) {
+  const { refreshToken } = req.body;
+
+  try {
+    jwt.verify(refreshToken, refreshSecret)
+  } catch (e) {
+
+  }
 });
 
 module.exports = router;
