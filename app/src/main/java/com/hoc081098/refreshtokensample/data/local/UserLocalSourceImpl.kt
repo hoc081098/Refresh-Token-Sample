2ade4b19b362879501a6fbbc00e9f873d6fae741
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2022-05-26 23:47:20 +0700

    update UserLocalSourceImpl

--- app/src/main/java/com/hoc081098/refreshtokensample/data/local/UserLocalSourceImpl.kt
+++ app/src/main/java/com/hoc081098/refreshtokensample/data/local/UserLocalSourceImpl.kt
@@ -1,15 +1,20 @@
 package com.hoc081098.refreshtokensample.data.local
 
 import androidx.datastore.core.DataStore
+import com.hoc081098.refreshtokensample.AppDispatchers
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 import java.io.IOException
 import javax.inject.Inject
 
-class UserLocalSourceImpl @Inject constructor(private val dataStore: DataStore<UserLocal>) :
-  UserLocalSource {
+class UserLocalSourceImpl @Inject constructor(
+  private val dataStore: DataStore<UserLocal>,
+  private val appDispatchers: AppDispatchers,
+) : UserLocalSource {
   override fun user() = dataStore.data
     .onEach { Timber.d("userLocal=$it") }
     .map { v -> v.takeIf { it != USER_LOCAL_NULL } }
@@ -20,9 +25,12 @@ class UserLocalSourceImpl @Inject constructor(private val dataStore: DataStore<U
         throw cause
       }
     }
+    .flowOn(appDispatchers.io)
 
   override suspend fun update(transform: suspend (current: UserLocal?) -> UserLocal?) =
-    dataStore.updateData { current ->
-      transform(current.takeIf { it != USER_LOCAL_NULL }) ?: USER_LOCAL_NULL
+    withContext(appDispatchers.io) {
+      dataStore.updateData { current ->
+        transform(current.takeIf { it != USER_LOCAL_NULL }) ?: USER_LOCAL_NULL
+      }
     }
 }
